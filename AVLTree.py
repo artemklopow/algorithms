2c595ef652ce6757f563dee5284e1ccfe0cd74e3
Artem Klopov <artemklopow@gmail.com>
2018-10-21 11:48:48 +0300

    Add balance to AVL tree

--- AVLTree.py
+++ AVLTree.py
@@ -1,19 +1,18 @@
-class Node:
-    """Node of AVL tree"""
-
-    def __init__(self, value):
-        self.value = value
-        self.height = 0
-        self.left_son = None
-        self.right_son = None
-        self.parent = None
-        self.is_parent_left = False
-
-    def is_root(self, tree):
-        return self is tree.root
-
-
 class AVLTree:
+
+    class Node:
+        """Node of AVL tree"""
+
+        def __init__(self, value):
+            self.value = value
+            self.height = 1
+            self.left_son = None
+            self.right_son = None
+            self.parent = None
+            self.is_parent_left = False
+
+        def is_root(self, tree):
+            return self is tree.root
 
     def __init__(self):
         self.root = None
@@ -46,12 +45,11 @@ class AVLTree:
         # Indicator means from which side we`re came to none reference.
         indicator = None
 
-        parent_node = Node
+        parent_node = None
         now_node = self.root
 
         # Looking for None reference...
         while now_node is not None:
-            now_node.height += 1
             parent_node = now_node
             if ins_node.value < now_node.value:
                 indicator = 'l'
@@ -71,6 +69,21 @@ class AVLTree:
             parent_node.right_son = ins_node
             ins_node.parent = parent_node
             ins_node.is_parent_left = True
+
+        self.edit_height(ins_node)
+        self.node_balance(ins_node)
+
+    @staticmethod
+    def edit_height(node: Node):
+        """Edits height from node to root"""
+
+        now_node = node
+        while now_node is not None:
+            left_height = 0 if now_node.left_son is None else now_node.left_son.height
+            right_height = 0 if now_node.right_son is None else now_node.right_son.height
+            now_height = (max(left_height, right_height)) + 1
+            now_node.height = now_height
+            now_node = now_node.parent
 
     @staticmethod
     def change(top_node: Node, low_node: Node):
@@ -113,6 +126,8 @@ class AVLTree:
         if low_right_son is not None:
             low_right_son.parent = top_node
 
+        top_node.height, low_node.height = low_node.height, top_node.height
+
     def delete(self, del_node: Node):
 
         # If node don`t has any sons, just remove it.
@@ -153,7 +168,149 @@ class AVLTree:
                 son_node.is_parent_left = False
                 parent_node.left_son = son_node
 
+        self.edit_height(parent_node)
+        self.node_balance(parent_node)
         self.__nodes.remove(del_node)
 
-# TODO
-# ВЫСОТЫ
+    def small_right_rotation(self, a: Node, b: Node):
+        a_parent = a.parent
+        a_is_parent_left = a.is_parent_left
+
+        C = b.right_son
+        if C is not None:
+            C.parent = a
+            C.is_parent_left = False
+        a.left_son = C
+        a.parent = b
+        a.is_parent_left = True
+
+        b.parent = a_parent
+        b.is_parent_left = a_is_parent_left
+        b.right_son = a
+
+        if a_parent is not None and a_is_parent_left:
+            a_parent.right_son = b
+        elif a_parent is not None and not a_is_parent_left:
+            a_parent.left_son = b
+
+        self.edit_height(a)
+
+    def small_left_rotation(self, a: Node, b: Node):
+        a_parent = a.parent
+        a_is_parent_left = a.is_parent_left
+
+        C = b.left_son
+        if C is not None:
+            C.parent = a
+            C.is_parent_left = True
+        a.right_son = C
+        a.parent = b
+        a.is_parent_left = False
+
+        b.parent = a_parent
+        b.is_parent_left = a_is_parent_left
+        b.left_son = a
+
+        if a_parent is not None and a_is_parent_left:
+            a_parent.right_son = b
+        elif a_parent is not None and not a_is_parent_left:
+            a_parent.left_son = b
+
+        self.edit_height(a)
+
+    def big_right_rotation(self, a: Node, b: Node, c: Node):
+        a_parent = a.parent
+        a_is_parent_left = a.is_parent_left
+
+        M = c.left_son
+        N = c.right_son
+
+        if M is not None:
+            M.parent = b
+            M.is_parent_left = True
+        if N is not None:
+            N.parent = a
+            N.is_parent_left = False
+
+        b.right_son = M
+        b.parent = c
+        b.is_parent_left = False
+
+        a.left_son = N
+        a.parent = c
+        a.is_parent_left = True
+
+        c.left_son = b
+        c.right_son = a
+        c.parent = a_parent
+        c.is_parent_left = a_is_parent_left
+
+        if a_parent is not None and a_is_parent_left:
+            a_parent.right_son = c
+        elif a_parent is not None and not a_is_parent_left:
+            a_parent.left_son = c
+
+        self.edit_height(a)
+        self.edit_height(b)
+
+    def big_left_rotation(self, a:Node, b: Node, c: Node):
+        a_parent = a.parent
+        a_is_parent_left = a.is_parent_left
+
+        M = c.left_son
+        N = c.right_son
+
+        if M is not None:
+            M.parent = a
+            M.is_parent_left = True
+        if N is not None:
+            N.parent = b
+            N.is_parent_left = False
+
+        b.left_son = N
+        b.parent = c
+        b.is_parent_left = True
+
+        a.right_son = M
+        a.parent = c
+        a.is_parent_left = False
+
+        c.left_son = a
+        c.right_son = b
+        c.parent = a_parent
+        c.is_parent_left = a_is_parent_left
+
+        if a_parent is not None and a_is_parent_left:
+            a_parent.right_son = c
+        elif a_parent is not None and not a_is_parent_left:
+            a_parent.left_son = c
+
+        self.edit_height(a)
+        self.edit_height(b)
+
+    def node_balance(self, node: Node):
+        now_node = node
+        while now_node is not None:
+            a = now_node
+            left_heigh = 0 if now_node.left_son is None else now_node.left_son.height
+            right_height = 0 if now_node.right_son is None else now_node.right_son.height
+            if left_heigh - right_height == 2:
+                b = a.left_son
+                b_left_height = 0 if b.left_son is None else b.left_son.height
+                b_right_height = 0 if b.right_son is None else b.right_son.height
+                if b_right_height <= b_left_height:
+                    self.small_right_rotation(a, b)
+                else:
+                    c = b.right_son
+                    self.big_right_rotation(a, b, c)
+            elif right_height - left_heigh == 2:
+                b = a.right_son
+                b_left_height = 0 if b.left_son is None else b.left_son.height
+                b_right_height = 0 if b.right_son is None else b.right_son.height
+                if b_left_height <= b_right_height:
+                    self.small_left_rotation(a, b)
+                else:
+                    c = b.left_son
+                    self.big_left_rotation(a, b, c)
+            else:
+                break
